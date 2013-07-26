f54392d9a3277929a07e21e2e59449924c10b013
Michel Kuhlmann <michel@kuhlmanns.info>
2013-07-26 07:57:51 +0200

    Adding more opportunities to pass in data

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 Simple command-line tool for string-template-replacement, based on Johan
 Tibbel's [template](https://github.com/tibbe/template)-library.
 
-    cat dd.tmpl
+The program could be run in three ways:
+
+    cat tt.tmpl
     The quick $color fox jumps over the $adj dog.
 
-    tmpl color=brown:adj=lazy < dd.tmpl
-    The quick brown fox jumps over the lazy dog.
+    cat dd.conf
+    color = brown
+    adj = lazy
+
+1. `tmpl color=brown:adj=lazy < tt.tmpl`
+2. `tmpl tt.tmpl < dd.conf`
+3. `tmpl tt.tmpl dd.conf`
--- tmpl.hs
+++ tmpl.hs
@@ -6,29 +6,59 @@ import qualified Data.ByteString.Lazy as S
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Lazy.Encoding as E
+import System.Directory (doesFileExist)
 
 import Data.Text.Template
 
+usage :: String
+usage = unlines ["Usage:"
+               ,"\ttmpl var1=uno:var2=dos < myfile.tmpl > myfile.txt"
+               ,"\ttmpl myfile.tmpl  < vars.conf > myfile.txt"
+               ,"\ttmpl myfile.tmpl vars.conf  > myfile.txt"
+               ]
 
 main :: IO ()
 main = do
   args <- getArgs
-  when (length args /= 1) (error usage)
-  let cntx = context . splitStr . head  $ args
-  tmpl <- TIO.getContents
+  (tmpl,cntx) <-
+      case length args of
+        1 -> do
+          let f = head args
+          isFile <- doesFileExist f
+          if isFile
+             then do
+               cntx <- fmap (context . splitStr) TIO.getContents
+               tmpl <- TIO.readFile f
+               return (tmpl,cntx)
+             else do
+               let cntx = context . splitStr . T.pack $ f
+               tmpl <- TIO.getContents
+               return (tmpl,cntx)
+
+        2 -> do
+          let tmplF:cntxF:[] = args
+          existTmplF <- doesFileExist tmplF
+          existCntxF <- doesFileExist cntxF
+          when (not  existTmplF || not existCntxF) (fail usage)
+          tmpl <- TIO.readFile tmplF
+          cntx <- fmap (context . splitStr) $ TIO.readFile cntxF
+          return (tmpl,cntx)
+
+        _ -> error usage
+
   S.putStr $ E.encodeUtf8 $ substitute tmpl cntx
 
-usage :: String
-usage = "Usage: tmpl var1=uno:var2=dos < myfile.tmpl > myfile.txt"
 
 -- | Create 'Context' from association list.
 context :: [(T.Text, T.Text)] -> Context
 context assocs x = maybe err id . lookup x $ assocs
   where err = error $ "Could not find key: " ++ T.unpack x
 
-splitStr :: String -> [(T.Text, T.Text)]
-splitStr s = map splitEq (T.split (== ':') (T.pack s))
+-- | First split Text on ':' or newline; second split on '='
+splitStr :: T.Text -> [(T.Text, T.Text)]
+splitStr s = map splitEq (T.split (\c -> c `elem` [':','\n'] ) s)
   where
     splitEq s = (k,v)
       where
-        k:v:[] = T.split (== '=') s
+        k:v:[] = map chomp $ T.split (== '=') s
+    chomp = T.replace " " ""
